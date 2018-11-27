0a96ee3a1d31490485c6f9a3afe4cb7411e1447a
kretschmer <jannes.kretschmer@heroes.eu>
2018-11-27 22:45:31 +0100

    added hibernate config

---
+++ src/main/java/de/lh/tool/config/HibernateConfiguragion.java
@@ -0,0 +1,73 @@
+package de.lh.tool.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("de.lh.tool.repository")
+@PropertySource(value = { "classpath:hibernate.properties" })
+public class HibernateConfiguragion {
+
+	@Autowired
+	private Environment environment;
+
+	@Bean(name = "entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
+		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		localContainerEntityManagerFactoryBean.setJpaVendorAdapter(getJpaVendorAdapter());
+		localContainerEntityManagerFactoryBean.setDataSource(getDataSource());
+		localContainerEntityManagerFactoryBean.setPersistenceUnitName("jpaPersistenceUnit");
+		localContainerEntityManagerFactoryBean.setPackagesToScan("de.lh.tool");
+		localContainerEntityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+		return localContainerEntityManagerFactoryBean;
+	}
+
+	@Bean
+	public JpaVendorAdapter getJpaVendorAdapter() {
+		JpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		return adapter;
+	}
+
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager getPlatformTransactionManager() {
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(
+				getEntityManagerFactoryBean().getObject());
+		return jpaTransactionManager;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driver"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+		return dataSource;
+	}
+
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		return properties;
+	}
+
+}
