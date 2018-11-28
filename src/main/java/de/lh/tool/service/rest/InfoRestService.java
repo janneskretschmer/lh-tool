1640eecb6a44534a66e41639354f4ce00a4db2f7
kretschmer <jannes.kretschmer@heroes.eu>
2018-11-28 01:01:48 +0100

    updated package declaration

--- src/main/java/de/lh/tool/service/rest/InfoRestService.java
+++ src/main/java/de/lh/tool/service/rest/InfoRestService.java
@@ -1,4 +1,4 @@
-package de.lh.tool.rest;
+package de.lh.tool.service.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
