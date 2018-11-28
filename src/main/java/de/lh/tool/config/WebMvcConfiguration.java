3c6c3135edb4b3bd6e1ecdacd80e701df6f512f9
Paul Hofmann <p.h.o@web.de>
2018-11-28 01:52:51 +0100

    Removed unused import

--- src/main/java/de/lh/tool/config/WebMvcConfiguration.java
+++ src/main/java/de/lh/tool/config/WebMvcConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
