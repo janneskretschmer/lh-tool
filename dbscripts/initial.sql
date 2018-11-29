50cce39fbebc07671ec4c8bca83cfb2ba5cd9efa
kretschmer <jannes.kretschmer@heroes.eu>
2018-11-29 13:27:03 +0100

    added initial db-script

---
+++ dbscripts/initial.sql
@@ -0,0 +1,18 @@
+-- When you change the content of this file, you have to update the checksume of the dbscripts entry as well!
+-- Please copy and execute the following commands manually as root:
+
+CREATE DATABASE IF NOT EXISTS lhtool;
+
+CREATE USER IF NOT EXISTS '<name>'@'localhost' IDENTIFIED BY '<password>';
+
+GRANT ALL ON lhtool.* TO '<name>'@'localhost';
+
+USE lhtool;
+
+CREATE TABLE `lhtool`.`dbscript` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `checksum` VARCHAR(32) NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
