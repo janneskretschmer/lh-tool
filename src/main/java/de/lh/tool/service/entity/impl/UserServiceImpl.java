531afa4565e02b6c1e0973dc85dd0b8a48c7de91
kretschmer <jannes.kretschmer@heroes.eu>
2018-12-07 01:39:23 +0100

    removed findUserByEmail

--- src/main/java/de/lh/tool/service/entity/impl/UserServiceImpl.java
+++ src/main/java/de/lh/tool/service/entity/impl/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package de.lh.tool.service.entity.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import de.lh.tool.bean.model.User;
@@ -11,8 +9,4 @@ import de.lh.tool.service.entity.interfaces.UserService;
 @Service
 public class UserServiceImpl extends BasicEntityServiceImpl<UserRepository, User, Long> implements UserService {
 
-	@Override
-	public List<User> findByEmail(String email) {
-		return getRepository().findByEmail(email);
-	}
 }
--- src/main/java/de/lh/tool/service/entity/interfaces/UserService.java
+++ src/main/java/de/lh/tool/service/entity/interfaces/UserService.java
@@ -1,11 +1,7 @@
 package de.lh.tool.service.entity.interfaces;
 
-import java.util.List;
-
 import de.lh.tool.bean.model.User;
 
 public interface UserService extends BasicEntityService<User, Long> {
-
-	List<User> findByEmail(String email);
 
 }
