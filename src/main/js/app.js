a111bbd51b1b32f3fba3b3148d513522289049b3
Paul Hofmann <p.h.o@web.de>
2019-01-28 00:04:30 +0100

    Added login-provider

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -5,26 +5,33 @@ import { fullPathOfHome, fullPathOfLogin, fullPathOfHeartbeat } from './paths';
 import HomeComponent from './components/home';
 import LoginComponent from './components/login';
 import HeartbeatComponent from './components/heartbeat';
+import LoginProvider, { LoginContext } from './providers/login-provider';
 
 const LHToolApp = () => (
 	<Router>
 		<div>
-			<h1>LH-Tool</h1>
-			<h3>Local Helper Tool for LDC</h3>
+			<LoginProvider>
+				<h1>LH-Tool</h1>
+				<h3>Local Helper Tool for LDC</h3>
 
-			<Helmet>
-				<title>LH-Tool</title>
-			</Helmet>
+				<Helmet>
+					<title>LH-Tool</title>
+				</Helmet>
 
-			<ul>
-				<li><Link to={fullPathOfHome()}>Home</Link></li>
-				<li><Link to={fullPathOfLogin()}>Login</Link></li>
-				<li><Link to={fullPathOfHeartbeat()}>Heartbeat</Link></li>
-			</ul>
+				<ul>
+					<li><Link to={fullPathOfHome()}>Home</Link></li>
+					<li><Link to={fullPathOfLogin()}>Login</Link></li>
+					<li><Link to={fullPathOfHeartbeat()}>Heartbeat</Link></li>
+				</ul>
 
-			<Route path={fullPathOfHome()} exact component={HomeComponent} />
-			<Route path={fullPathOfLogin()} component={LoginComponent} />
-			<Route path={fullPathOfHeartbeat()} component={HeartbeatComponent} />
+				<LoginContext.Consumer>
+					{loginState => loginState.isLoggedIn() ? 'ANGEMELDET' : 'NICHT ANGEMELDET'}
+				</LoginContext.Consumer>
+
+				<Route path={fullPathOfHome()} exact component={HomeComponent} />
+				<Route path={fullPathOfLogin()} component={LoginComponent} />
+				<Route path={fullPathOfHeartbeat()} component={HeartbeatComponent} />
+			</LoginProvider>
 		</div>
 	</Router>
 );
--- src/main/js/components/login.js
+++ src/main/js/components/login.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { LoginContext } from '../providers/login-provider';
+import { apiRequest, apiEndpoints } from '../apiclient';
 
 const LoginComponent = () => (
-    <form>
-        <Helmet titleTemplate="Login - %s" />
-        <label htmlFor="username">
-            Nutzername
-        </label>
-        <input type="text" name="username" />
-        <br />
-        <label htmlFor="password">Passwort</label>
-        <input type="password" name="password" />
-        <button type="submit">
-            Anmelden
-        </button>
-    </form>
+    <LoginContext.Consumer>
+        {loginState => (
+            <form onSubmit={evt => {
+                evt.preventDefault();
+                // TODO Actual login logic
+            }}>
+                <Helmet titleTemplate="Login - %s" />
+                <label htmlFor="username">
+                    Nutzername
+                </label>
+                <input type="text" name="username" />
+                <br />
+                <label htmlFor="password">Passwort</label>
+                <input type="password" name="password" />
+                <button type="submit">
+                    Anmelden
+                </button>
+            </form>
+        )}
+    </LoginContext.Consumer>
 )
 
 export default LoginComponent;
