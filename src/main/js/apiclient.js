cd01a1dec827e6a80c8a858cd8c7ca9accf24f3a
Paul Hofmann <p.h.o@web.de>
2019-02-05 23:35:03 +0100

    Code smell fixes

--- src/main/js/apiclient.js
+++ src/main/js/apiclient.js
@@ -15,30 +15,33 @@ export function apiRequest({
     parameters = {},
     data
 } = {
-        apiEndpoint: undefined,
-        authToken: undefined,
+        apiEndpoint: null,
+        authToken: null,
         queries: {},
         parameters: {},
-        data: undefined
+        data: null,
     }) {
     const apiHostUrlTemplate = new URI('/');
 
-    if (!apiEndpoint || (!apiEndpoint.method) || (!apiEndpoint.path))
+    if (!apiEndpoint || (!apiEndpoint.method) || (!apiEndpoint.path)) {
         return Promise.reject(new Error('No apiEndpoint of form {method,path} given.'));
+    }
 
     if ('parameters' in apiEndpoint) {
         const parKeys = Object.keys(parameters);
         const parDiff = (apiEndpoint.parameters || []).filter(par => !parKeys.includes(par));
-        if (parDiff.length > 0)
+        if (parDiff.length > 0) {
             return Promise.reject(new Error(`Missing parameters: ${JSON.stringify(parDiff)}`));
+        }
     }
 
     const renderedPath = URI.expand(getContextPath() + apiEndpoint.path, parameters);
 
     let pendingReq = superagent(apiEndpoint.method, apiHostUrlTemplate.path(renderedPath).toString()).type('json');
 
-    if (authToken)
+    if (authToken) {
         pendingReq = pendingReq.set('Authorization', `Bearer ${authToken}`);
+    }
 
     Object.keys(queries).forEach(queryKey => {
         pendingReq = pendingReq.query({
@@ -46,26 +49,30 @@ export function apiRequest({
         });
     });
 
-    if (data)
+    if (data) {
         pendingReq = pendingReq.send(data);
+    }
 
     return new Promise((resolve, reject) => {
         pendingReq.end((err, res) => {
-            if (err)
+            if (err) {
                 reject({
                     error: err,
                     response: res ? (res.body || null) : null
                 });
-            else if (!res.body)
+            }
+            else if (!res.body) {
                 reject({
                     error: new Error('No valid data received.'),
                     response: null
                 });
-            else
+            }
+            else {
                 resolve({
                     error: null,
                     response: res.body
                 });
+            }
         });
     });
 }
@@ -84,6 +91,6 @@ export const apiEndpoints = {
             path: LOGIN_PEFIX + '/',
         },
     },
-    project: undefined,
-    user: undefined,
+    project: null,
+    user: null,
 };
