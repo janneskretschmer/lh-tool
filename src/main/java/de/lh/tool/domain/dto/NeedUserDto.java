6a30c064bd5cffeefc568a1ab0977f94446981a9
kretschmer <jannes.kretschmer@heroes.eu>
2019-02-14 23:48:08 +0100

    added NeedUser-tests

--- src/main/java/de/lh/tool/domain/dto/NeedUserDto.java
+++ src/main/java/de/lh/tool/domain/dto/NeedUserDto.java
@@ -1,13 +1,22 @@
 package de.lh.tool.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import de.lh.tool.domain.model.NeedUserState;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class NeedUserDto {
 
 	private Long id;
 	private Long needId;
 	private Long userId;
+	@NonNull
 	private NeedUserState state;
 }
