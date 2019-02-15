dd0554290e63283de024f97eafa9eae0c410f996
Paul Hofmann <p.h.o@web.de>
2019-02-15 04:14:49 +0100

    Removed unused imort

--- src/main/js/components/project-create.js
+++ src/main/js/components/project-create.js
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import { ProjectsContext } from '../providers/projects-provider';
 import { SessionContext } from '../providers/session-provider';
 import { createNewProject } from '../actions/project';
