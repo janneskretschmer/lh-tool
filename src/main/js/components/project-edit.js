3483c83f9dc7f8da64fdba7400e074c3500d95f3
kretschmer <jannes.kretschmer@heroes.eu>
2019-02-19 18:59:16 +0100

    started project edit component

--- src/main/js/components/project-edit.js
+++ src/main/js/components/project-edit.js
@@ -12,7 +12,15 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { ProjectsContext } from '../providers/projects-provider';
 import { SessionContext } from '../providers/session-provider';
 import { deleteProject } from '../actions/project';
+<<<<<<< Upstream, based on origin/master
 import WithPermission from './with-permission';
+=======
+import Typography from '@material-ui/core/Typography';
+
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+>>>>>>> ef8c21b started project edit component
 
 const styles = theme => ({
     root: {
@@ -30,7 +38,119 @@ const styles = theme => ({
 
 const Transition = props => (<Slide direction="up" {...props} />);
 
+<<<<<<< Upstream, based on origin/master
 @withSnackbar
+=======
+const UserComponent = props => {
+    let inputFirstName = null;
+    let inputLastName = null;
+    let inputEmail = null;
+    let inputIsFemale = null;
+    let inputTelephoneBusiness = null;
+    let inputTelephoneMobile = null;
+    let inputTelephoneHome = null;
+    
+    return (
+        <div>
+            <TextField
+                id="first_name"
+                label="Vorname"
+                type="text"
+                name="first_name"
+                autoComplete="first name"
+                margin="dense"
+                variant="outlined"
+                InputProps={{
+                    inputRef: ref => inputFirstName = ref
+                }}
+                required
+            />
+            <TextField
+                id="last_name"
+                label="Nachname"
+                type="text"
+                name="last_name"
+                autoComplete="last name"
+                margin="dense"
+                variant="outlined"
+                InputProps={{
+                    inputRef: ref => inputLastName = ref
+                }}
+                required
+            />
+            <TextField
+                id="email"
+                label="Email"
+                type="email"
+                name="email"
+                autoComplete="email"
+                margin="dense"
+                variant="outlined"
+                InputProps={{
+                    inputRef: ref => inputEmail = ref
+                }}
+                required
+            /><br/>
+            <TextField
+                id="telephone_home"
+                label="Telefon Festnetz"
+                type="text"
+                name="telephone_home"
+                autoComplete="telephone home"
+                margin="dense"
+                variant="outlined"
+                InputProps={{
+                    inputRef: ref => inputTelephoneHome = ref
+                }}
+            />
+            <TextField
+                id="telephone_mobile"
+                label="Telefon Mobil"
+                type="text"
+                name="telephone_mobile"
+                autoComplete="telephone mobile"
+                margin="dense"
+                variant="outlined"
+                InputProps={{
+                    inputRef: ref => inputTelephoneMobile = ref
+                }}
+            />
+            <TextField
+                id="telephone_business"
+                label="Telefon Geschäftlich"
+                type="text"
+                name="telephone_business"
+                autoComplete="telephone business"
+                margin="dense"
+                variant="outlined"
+                InputProps={{
+                    inputRef: ref => inputTelephoneBusiness = ref
+                }}
+            /><br></br>
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        checked={props.user && props.user.gender == 'FEMALE'}
+                
+                        value="gender"
+                        inputProps={{
+                            ref: ref => inputIsFemale = ref
+                        }}
+                    />
+                }
+                label="Schwester"
+            />
+            <Button size="small" color="secondary">
+                Abbrechen
+            </Button>
+            <Button variant="contained" type="submit">
+                Speichern
+            </Button>
+        </div>
+    )
+}
+
+>>>>>>> ef8c21b started project edit component
 @withStyles(styles)
 export default class ProjectEditPanel extends React.Component {
 
@@ -63,7 +183,15 @@ export default class ProjectEditPanel extends React.Component {
                     {sessionState => (
                         <ProjectsContext.Consumer>
                             {projectsState => (
+<<<<<<< Upstream, based on origin/master
                                 <WithPermission permission="ROLE_RIGHT_PROJECTS_DELETE">
+=======
+                                <>
+                                    <div><Typography variant="h6">Baudiener</Typography></div>
+                                    <div><Typography variant="h6">Lokaler Koordinator</Typography></div>
+                                    <UserComponent/>
+                                    <div><Typography variant="h6">Verkündiger</Typography></div>
+>>>>>>> ef8c21b started project edit component
                                     <Button variant="contained" color="primary" className={classes.button} onClick={this.handleDeleteButtonClicked.bind(this)}>
                                         Projekt löschen
                                         <DeleteIcon className={classes.rightIcon} />
