caeb962ae2bb216289a89d182a170a3afa5d2436
kretschmer <jannes.kretschmer@heroes.eu>
2019-02-23 23:46:27 +0100

    show more information of user

--- src/main/js/components/user-detail.js
+++ src/main/js/components/user-detail.js
@@ -6,41 +6,33 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
     input: {
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit,
     },
-    userName: {
+    bold: {
         fontWeight: 'bold',
+    },
+    userData: {
+        maxHeight: '150px',
+        overflow: 'auto',
     }
 });
 
 const UserComponent = props => {
 
-    const { classes, user, role, saveHandler, showEdit } = props;
+    const { classes, role, saveHandler, showEdit } = props;
 
-    var firstName = user ? user.firstName : null;
-    var lastName = user ? user.lastName : null;
-    var email = user ? user.email : null;
-    var telephoneNumber = user ? user.telephoneNumber : null;
-    var mobileNumber = user ? user.mobileNumber : null;
-    var businessNumber = user ? user.businessNumber : null;
-    var isFemale = user && user.gender === 'FEMALE';
-
-    const [edit, setEdit] = useState(!user);
+    const [edit, setEdit] = useState(!props.user);
+    const [user, setUser] = useState(props.user ? props.user : {firstName:'', lastName:'',email:'',telephoneNumber:'',mobileNumber:'',businessNumber:''})
 
     const handleSave = () => {
         if (saveHandler) {
             saveHandler({
-                firstName,
-                lastName,
-                email,
-                gender: isFemale ? 'FEMALE' : 'MALE',
-                telephoneNumber,
-                mobileNumber,
-                businessNumber,
+                ...user,
                 role,
             })
         }
@@ -53,86 +45,86 @@ const UserComponent = props => {
                 (<>
                     <TextField
                         id="first_name"
-                        value={firstName}
+                        value={user.firstName}
                         label="Vorname"
                         type="text"
                         name="first_name"
                         autoComplete="first name"
                         margin="dense"
                         variant="outlined"
-                        onChange={e => firstName = e.target.value}
+                        onChange={e => setUser({...user,firstName:e.target.value})}
                         className={classes.input}
                         required
                     />
                     <TextField
                         id="last_name"
-                        value={lastName}
+                        value={user.lastName}
                         label="Nachname"
                         type="text"
                         name="last_name"
                         autoComplete="last name"
                         margin="dense"
                         variant="outlined"
-                        onChange={e => lastName = e.target.value}
+                        onChange={e => setUser({...user,lastName:e.target.value})}
                         className={classes.input}
                         required
                     />
                     <TextField
                         id="email"
-                        value={email}
+                        value={user.email}
                         label="Email"
                         type="email"
                         name="email"
                         autoComplete="email"
                         margin="dense"
                         variant="outlined"
-                        onChange={e => email = e.target.value}
+                        onChange={e => setUser({...user,email:e.target.value})}
                         className={classes.input}
                         required
                     /><br />
                     <TextField
                         id="telephone_number"
-                        value={telephoneNumber}
+                        value={user.telephoneNumber}
                         label="Telefon Festnetz"
                         type="text"
                         name="telephone_number"
                         autoComplete="telephone number"
                         margin="dense"
                         variant="outlined"
-                        onChange={e => telephoneNumber = e.target.value}
+                        onChange={e => setUser({...user,telephoneNumber:e.target.value})}
                         className={classes.input}
                     />
                     <TextField
                         id="mobile_number"
-                        value={mobileNumber}
+                        value={user.mobileNumber}
                         label="Telefon Mobil"
                         type="text"
                         name="mobile_number"
                         autoComplete="telephone mobile number"
                         margin="dense"
                         variant="outlined"
-                        onChange={e => mobileNumber = e.target.value}
+                        onChange={e => setUser({...user,mobileNumber:e.target.value})}
                         className={classes.input}
                     />
                     <TextField
                         id="business_number"
-                        value={businessNumber}
+                        value={user.businessNumber}
                         label="Telefon Geschäftlich"
                         type="text"
                         name="business_number"
                         autoComplete="telephone business number"
                         margin="dense"
                         variant="outlined"
-                        onChange={e => businessNumber = e.target.value}
+                        onChange={e => setUser({...user,businessNumber:e.target.value})}
                         className={classes.input}
                     /><br />
                     <FormControlLabel
                         control={
                             <Checkbox
-                                checked={isFemale}
+                                checked={user.gender == 'FEMALE'}
 
                                 value="gender"
-                                onChange={e => isFemale = e.target.value}
+                                onChange={e => setUser({...user,gender:e.target.checked?'FEMALE':'MALE'})}
                             />
                         }
                         label="Schwester"
@@ -148,12 +140,30 @@ const UserComponent = props => {
                 </>)
                 :
                 (<>
-                    <Typography variant="body1" className={classes.userName}>{user ? user.lastName : ''}, {user ? user.firstName : ''}
-                        {showEdit ?
-                            (<Button size="small" color="secondary" onClick={() => setEdit(true)}>
-                                <Icon>create</Icon>
-                            </Button>) : null}
-                    </Typography>
+                    <Grid container wrap="wrap" spacing={8}>
+                        <Grid item xs={6} sm={2} className={classes.userData}><span className={classes.bold}>{user ? user.lastName : ''}, {user ? user.firstName : ''}</span></Grid>
+                        <Grid item xs={6} sm={2} className={classes.userData}><a href={user?'mailto:'+user.email:''}>{user?user.email:''}</a></Grid>
+                        <Grid item xs={12} sm={3} container className={classes.userData}>
+                            <Grid item>
+                                Festnetz:<br/>
+                                Mobil:<br/>
+                                Geschäftlich:                                
+                            </Grid>
+                            <Grid item>
+                                <a href={user ? 'tel:'+user.telephoneNumber:''}>{user ? user.telephoneNumber:''}</a><br/>
+                                <a href={user ? 'tel:'+user.mobileNumber:''}>{user ? user.mobileNumber:''}</a><br/>
+                                <a href={user ? 'tel:'+user.businessNumber:''}>{user ? user.businessNumber:''}</a>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12}  sm={4} className={classes.userData}>
+                            <span className={classes.bold}>Beruf:</span> {user ? user.profession : ''}<br/>
+                            <span className={classes.bold}>Fähigkeiten:</span> {user ? user.skills:''}
+                        </Grid>
+                        <Grid item sm={1} className={classes.userData}>{showEdit || true ?
+                        (<Button size="small" color="secondary" onClick={() => setEdit(true)}>
+                            <Icon>create</Icon>
+                        </Button>) : null}</Grid>                            
+                    </Grid>
 
                 </>)
             }
