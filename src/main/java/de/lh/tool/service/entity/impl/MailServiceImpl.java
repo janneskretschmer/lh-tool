ea8b6f5dd3eb130bb83a204be0eaf21ee7e694ca
kretschmer <jannes.kretschmer@heroes.eu>
2019-02-23 21:07:58 +0100

    prepared mails for local coordinator and publisher

--- src/main/java/de/lh/tool/service/entity/impl/MailServiceImpl.java
+++ src/main/java/de/lh/tool/service/entity/impl/MailServiceImpl.java
@@ -13,10 +13,16 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import de.lh.tool.domain.model.User;
 import de.lh.tool.service.entity.interfaces.MailService;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Service
 public class MailServiceImpl implements MailService {
+
+	private static final String SENDER_NAME = "LDC Baugruppe 5";
+	private static final String FOOTER = "p.s. Das ist eine automatisch generierte Mail, bitte antworte nicht darauf. Bei Fragen wende dich bitte an ...";
 
 	private Properties properties;
 
@@ -38,13 +44,55 @@ public class MailServiceImpl implements MailService {
 	}
 
 	@Override
-	public void test() {
-		System.out.println("Host: " + host);
-		sendMail(username, password, username, "jannes.kretschmer@gmx.de", "Test", "sers, das ist ein Test xD");
+	public void sendNewLocalCoordinatorMail(User user) {
+		if (user != null) {
+			if (user.getEmail() != null) {
+				StringBuilder text = new StringBuilder("Lieber Bruder ").append(user.getLastName()).append(
+						",\n\nes wurde für dich ein Account auf lh-tool.de angelegt. Diese Webseite hilft dir als lokalen Koordinator, den Bedarf an Helfern zu verwalten.\n")
+						.append("Bitte rufe folgenden Link auf, um ein Passwort zu setzen. Anschließend kannst du Accounts für alle Brüder und Schwestern erstellen, welche geeignet sind, bei der Baustelle mitzuhelfen.\n\n")
+						.append("TODO: Link generieren" + user.getPasswordChangeToken())
+						.append("\n\nVielen Dank für deine Bereitschaft. Wir wünschen dir Jehovas Segen bei deiner Aufgabe.\n\nIn brüderlicher Liebe\n")
+						.append(SENDER_NAME).append("\n\n").append(FOOTER);
+				sendMail(user.getEmail(), "Account bei lh-tool.de", text.toString());
+				if (log.isInfoEnabled()) {
+					log.info("Mail for local coordinator " + user.getFirstName() + " " + user.getLastName()
+							+ " sent to " + user.getEmail());
+				}
+			} else {
+				if (log.isInfoEnabled()) {
+					log.warn("Mail for local coordinator " + user.getFirstName() + " " + user.getLastName()
+							+ " with id " + user.getId() + " not sent");
+				}
+			}
+		}
 	}
 
-	private void sendMail(String username, String password, String fromEmailAddress, String toEmailAddress,
-			String subject, String messageText) {
+	@Override
+	public void sendNewPublisherMail(User user) {
+		if (user != null) {
+			if (user.getEmail() != null) {
+				StringBuilder text = new StringBuilder(User.Gender.FEMALE.equals(user.getGender()) ? "Liebe Schwester "
+						: "Lieber Bruder ").append(user.getLastName()).append(
+								",\n\nes wurde für dich ein Account auf lh-tool.de angelegt. Auf dieser Webseite kannst du dich als Helfer bei der Baustelle an deinem Saal bewerben.\n")
+								.append("Bitte rufe folgenden Link auf, um ein Passwort zu setzen. Anschließend kannst du angeben, an welchen Tagen es dir möglich wäre mitzuhelfen.\n\n")
+								.append("TODO: Link generieren" + user.getPasswordChangeToken())
+								.append("\n\nVielen Dank für deine Bereitschaft. Wir wünschen dir Jehovas Segen.\n\nIn brüderlicher Liebe\n")
+								.append(SENDER_NAME).append("\n\n").append(FOOTER);
+				sendMail(user.getEmail(), "Account bei lh-tool.de", text.toString());
+				if (log.isInfoEnabled()) {
+					log.info("Mail for local coordinator " + user.getFirstName() + " " + user.getLastName()
+							+ " sent to " + user.getEmail());
+				}
+			} else {
+				if (log.isInfoEnabled()) {
+					log.warn("Mail for local coordinator " + user.getFirstName() + " " + user.getLastName()
+							+ " with id " + user.getId() + " not sent");
+				}
+			}
+		}
+	}
+
+	private void sendMail(String toEmailAddress, String subject, String messageText) {
 		properties.put("mail.smtp.host", host);
 		Session session = Session.getInstance(properties, new Authenticator() {
 			@Override
@@ -54,13 +102,13 @@ public class MailServiceImpl implements MailService {
 		});
 		try {
 			Message msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress(fromEmailAddress));
+			msg.setFrom(new InternetAddress(SENDER_NAME + "<" + username + ">"));
 			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmailAddress));
 			msg.setSubject(subject);
 			msg.setText(messageText);
 			Transport.send(msg);
-		} catch (Exception ex) {
-			ex.printStackTrace();
+		} catch (Exception e) {
+			log.warn("Email to " + toEmailAddress + " could not be sent", e);
 		}
 	}
 }
--- src/main/java/de/lh/tool/service/entity/interfaces/MailService.java
+++ src/main/java/de/lh/tool/service/entity/interfaces/MailService.java
@@ -1,7 +1,11 @@
 package de.lh.tool.service.entity.interfaces;
 
+import de.lh.tool.domain.model.User;
+
 public interface MailService {
 
-	void test();
+	void sendNewLocalCoordinatorMail(User user);
+
+	void sendNewPublisherMail(User user);
 
 }
