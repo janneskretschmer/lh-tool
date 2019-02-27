9b4a6240afc194fa51527b63e921c449d7bb210a
kretschmer <jannes.kretschmer@heroes.eu>
2019-02-27 21:33:41 +0100

    fixed project-edit.js

--- src/main/js/components/project-edit.js
+++ src/main/js/components/project-edit.js
@@ -5,15 +5,11 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { ProjectsContext } from '../providers/projects-provider';
 import { SessionContext } from '../providers/session-provider';
 import { deleteProject } from '../actions/project';
-<<<<<<< HEAD
 import WithPermission from './with-permission';
 import Typography from '@material-ui/core/Typography';
 import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-=======
-import Typography from '@material-ui/core/Typography';
->>>>>>> branch 'project-edit' of https://github.com/janneskretschmer/lh-tool.git
 import UserComponent from './user-detail';
 import { createNewUser, updateUser, deleteUser } from '../actions/user'
 import SimpleDialog from './simple-dialog.js'
@@ -35,119 +31,7 @@ const styles = theme => ({
     },
 });
 
-<<<<<<< HEAD
 @withSnackbar
-const UserComponent = props => {
-    let inputFirstName = null;
-    let inputLastName = null;
-    let inputEmail = null;
-    let inputIsFemale = null;
-    let inputTelephoneBusiness = null;
-    let inputTelephoneMobile = null;
-    let inputTelephoneHome = null;
-    
-    return (
-        <div>
-            <TextField
-                id="first_name"
-                label="Vorname"
-                type="text"
-                name="first_name"
-                autoComplete="first name"
-                margin="dense"
-                variant="outlined"
-                InputProps={{
-                    inputRef: ref => inputFirstName = ref
-                }}
-                required
-            />
-            <TextField
-                id="last_name"
-                label="Nachname"
-                type="text"
-                name="last_name"
-                autoComplete="last name"
-                margin="dense"
-                variant="outlined"
-                InputProps={{
-                    inputRef: ref => inputLastName = ref
-                }}
-                required
-            />
-            <TextField
-                id="email"
-                label="Email"
-                type="email"
-                name="email"
-                autoComplete="email"
-                margin="dense"
-                variant="outlined"
-                InputProps={{
-                    inputRef: ref => inputEmail = ref
-                }}
-                required
-            /><br/>
-            <TextField
-                id="telephone_home"
-                label="Telefon Festnetz"
-                type="text"
-                name="telephone_home"
-                autoComplete="telephone home"
-                margin="dense"
-                variant="outlined"
-                InputProps={{
-                    inputRef: ref => inputTelephoneHome = ref
-                }}
-            />
-            <TextField
-                id="telephone_mobile"
-                label="Telefon Mobil"
-                type="text"
-                name="telephone_mobile"
-                autoComplete="telephone mobile"
-                margin="dense"
-                variant="outlined"
-                InputProps={{
-                    inputRef: ref => inputTelephoneMobile = ref
-                }}
-            />
-            <TextField
-                id="telephone_business"
-                label="Telefon Geschäftlich"
-                type="text"
-                name="telephone_business"
-                autoComplete="telephone business"
-                margin="dense"
-                variant="outlined"
-                InputProps={{
-                    inputRef: ref => inputTelephoneBusiness = ref
-                }}
-            /><br></br>
-            <FormControlLabel
-                control={
-                    <Checkbox
-                        checked={props.user && props.user.gender == 'FEMALE'}
-                
-                        value="gender"
-                        inputProps={{
-                            ref: ref => inputIsFemale = ref
-                        }}
-                    />
-                }
-                label="Schwester"
-            />
-            <Button size="small" color="secondary">
-                Abbrechen
-            </Button>
-            <Button variant="contained" type="submit">
-                Speichern
-            </Button>
-        </div>
-    )
-}
-
-=======
->>>>>>> branch 'project-edit' of https://github.com/janneskretschmer/lh-tool.git
 @withStyles(styles)
 export default class ProjectEditPanel extends React.Component {
 
@@ -163,15 +47,12 @@ export default class ProjectEditPanel extends React.Component {
             ...this.state,
             editPublishers: !this.state.editPublishers,
         })
-<<<<<<< HEAD
     }
 
     handleDeleteFailure() {
         this.props.enqueueSnackbar('Fehler beim Löschens des Projekts', {
             variant: 'error',
         });
-=======
->>>>>>> branch 'project-edit' of https://github.com/janneskretschmer/lh-tool.git
     }
 
     render() {
@@ -233,12 +114,9 @@ export default class ProjectEditPanel extends React.Component {
                                                     projectId: this.props.project.id,
                                                     handleFailure: this.handleDeleteFailure.bind(this),
                                                 });
-<<<<<<< HEAD
-                                            }}>
-=======
-                                            }}
-                                    >
->>>>>>> branch 'project-edit' of https://github.com/janneskretschmer/lh-tool.git
+                                            }
+                                    }>
+
                                         <Button variant="contained" color="primary" className={classes.button}>
                                             Projekt löschen
                                             <DeleteIcon className={classes.rightIcon} />
