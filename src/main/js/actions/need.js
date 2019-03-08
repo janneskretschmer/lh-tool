70772cef4c653b37d0a4005c171b789177567378
Paul Hofmann <p.h.o@web.de>
2019-03-09 00:13:49 +0100

    Format

--- src/main/js/actions/need.js
+++ src/main/js/actions/need.js
@@ -1,13 +1,13 @@
+import moment from 'moment';
 import { apiRequest, apiEndpoints } from '../apiclient';
 import { ID_VARIABLE } from '../urlmappings';
-import moment from 'moment';
 
 function mapNeedArray(content) {
     let needs = []
     content.forEach(need => {
         const date = moment(need.date, 'x');
-        let item = needs[needs.length -1];
-        if(item === undefined || !date.isSame(item.date,'day') || item.projectName !== need.projectName){
+        let item = needs[needs.length - 1];
+        if (item === undefined || !date.isSame(item.date, 'day') || item.projectName !== need.projectName) {
             item = {
                 date,
                 projectName: need.projectName,
@@ -38,10 +38,10 @@ export function createOrUpdateNeed({ accessToken, need, needsState, handleFailur
         apiEndpoint: need.id ? apiEndpoints.need.update : apiEndpoints.need.createNew,
         authToken: accessToken,
         data: need,
-        parameters:need.id ? {[ID_VARIABLE]:need.id} : {},
+        parameters: need.id ? { [ID_VARIABLE]: need.id } : {},
     })
         .then(result => {
-           needsState.needsUpdated(result.response);
+            needsState.needsUpdated(result.response);
         })
         .catch(err => {
             if (handleFailure) {
