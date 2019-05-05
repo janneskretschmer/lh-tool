d147b818b3a683dfb107e80fe9ad7b0ff33531b7
jannes <jannes.kretschmer@gmx.de>
2019-05-04 19:30:53 -0700

    converted tab to whitespace

--- src/main/js/components/need-list.js
+++ src/main/js/components/need-list.js
@@ -23,20 +23,20 @@ import Typography from '@material-ui/core/Typography';
 import moment from 'moment';
 
 const Date = props => (
-	<>{['Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'][props.date.format('E')-2]}, {props.date.format('DD.MM.YYYY')}</>
+  <>{['Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'][props.date.format('E')-2]}, {props.date.format('DD.MM.YYYY')}</>
 )
 
 const styles = theme => ({
-	projectWrapper: { 
-		marginRight: '50px',
-		display: 'inline',
-	},
+  projectWrapper: {
+  marginRight: '50px',
+  display: 'inline',
+  },
 });
 
 @withStyles(styles)
 @withSnackbar
 class StatefulNeedsComponent extends React.Component {
-	
+
     constructor(props) {
         super(props);
         this.state = {
@@ -48,139 +48,139 @@ class StatefulNeedsComponent extends React.Component {
             day: null,
         };
     }
-    
+
     enterSingleDayMode(day) {
-    	this.setState({
-    		...this.state,
-    		singleDayMode: true,
-    		day,
-    		needData: null,
-    	},this.loadNeedData);
+      this.setState({
+        ...this.state,
+        singleDayMode: true,
+        day,
+        needData: null,
+      },this.loadNeedData);
     }
-    
+
     leaveSingleDayMode() {
-    	this.setState({
-    		...this.state,
-    		singleDayMode: false,
-    		needData: null,
-    	},this.loadNeedData);
+      this.setState({
+        ...this.state,
+        singleDayMode: false,
+        needData: null,
+      },this.loadNeedData);
     }
-    
+
     loadNeedData() {
-    	const start = this.state.singleDayMode ? this.state.day.diff(moment().startOf('day'),'days') : this.state.months[this.state.month].startOffset;
-    	const end = this.state.singleDayMode ? this.state.day.diff(moment().startOf('day'),'days') : this.state.months[this.state.month].endOffset;
-    	fetchOwnNeeds({accessToken:this.props.sessionState.accessToken, userId:this.props.sessionState.currentUser.id, projectId: this.state.project.id, startDiff: start, endDiff: end}).then(result => this.setState({
-    		...this.state,
-    		needData: result,
-    	}))
+      const start = this.state.singleDayMode ? this.state.day.diff(moment().startOf('day'),'days') : this.state.months[this.state.month].startOffset;
+      const end = this.state.singleDayMode ? this.state.day.diff(moment().startOf('day'),'days') : this.state.months[this.state.month].endOffset;
+      fetchOwnNeeds({accessToken:this.props.sessionState.accessToken, userId:this.props.sessionState.currentUser.id, projectId: this.state.project.id, startDiff: start, endDiff: end}).then(result => this.setState({
+        ...this.state,
+        needData: result,
+      }))
     }
-    
+
     switchProject(project) {
-    	this.setState({
-    		...this.state,
-    		project,
-    		months: getMonthArrayWithOffsets(project.startDate,project.endDate),
-    		month: 0,
-    		needData: null,
-    	},this.loadNeedData);
+      this.setState({
+        ...this.state,
+        project,
+        months: getMonthArrayWithOffsets(project.startDate,project.endDate),
+        month: 0,
+        needData: null,
+      },this.loadNeedData);
     }
-    
+
     handleMonthSelect(event) {
-    	this.setState({
-    		...this.state,
-    		month:event.target.value,
-    		needData: null,
-    	},this.loadNeedData)
+      this.setState({
+        ...this.state,
+        month:event.target.value,
+        needData: null,
+      },this.loadNeedData)
     }
-    
+
     canGoLeft() {
-    	return this.state.singleDayMode ? this.state.day > this.state.project.startDate : this.state.month > 0;
+      return this.state.singleDayMode ? this.state.day > this.state.project.startDate : this.state.month > 0;
     }
-    
+
     canGoRight() {
-    	return this.state.singleDayMode ? this.state.day < this.state.project.endDate : this.state.months && this.state.month < this.state.months.length - 1;
+      return this.state.singleDayMode ? this.state.day < this.state.project.endDate : this.state.months && this.state.month < this.state.months.length - 1;
     }
-    
+
     handleLeft() {
-    	this.setState({
-    		...this.state,
-    		needData: null,
-    		month: this.state.month - (this.state.singleDayMode ? 0 : 1),
-    		day: this.state.singleDayMode ? this.state.day.add(this.state.day.format('E') === '2'?-3:-1, 'days') : null,
-    	},this.loadNeedData)
+      this.setState({
+        ...this.state,
+        needData: null,
+        month: this.state.month - (this.state.singleDayMode ? 0 : 1),
+        day: this.state.singleDayMode ? this.state.day.add(this.state.day.format('E') === '2'?-3:-1, 'days') : null,
+      },this.loadNeedData)
     }
-    
+
     handleRight() {
-    	this.setState({
-    		...this.state,
-    		needData: null,
-    		month: this.state.month + (this.state.singleDayMode ? 0 : 1),
-    		day: this.state.singleDayMode ? this.state.day.add(this.state.day.format('E') === '6'?3:1, 'days') : null,
-    	},this.loadNeedData)
+      this.setState({
+        ...this.state,
+        needData: null,
+        month: this.state.month + (this.state.singleDayMode ? 0 : 1),
+        day: this.state.singleDayMode ? this.state.day.add(this.state.day.format('E') === '6'?3:1, 'days') : null,
+      },this.loadNeedData)
     }
-    
+
     render() {
         const { classes, sessionState } = this.props;
         const { singleDayMode } = this.state;
         const tmp = (
-        	<>  
-        		<div className={classes.projectWrapper}>
-            		<ProjectSelection  onChange={project => this.switchProject(project)} accessToken={sessionState.accessToken} />
-            	</div>
-            	{this.state.project ? (
-                	<>
-                		{this.canGoLeft() ? (
-                			<IconButton onClick={this.handleLeft.bind(this)}>
+          <>
+            <div className={classes.projectWrapper}>
+                <ProjectSelection  onChange={project => this.switchProject(project)} accessToken={sessionState.accessToken} />
+              </div>
+              {this.state.project ? (
+                  <>
+                    {this.canGoLeft() ? (
+                      <IconButton onClick={this.handleLeft.bind(this)}>
                                 <NavigateBeforeIcon />
-                            </IconButton>	
-                		) : null}
-                		{this.state.singleDayMode ? (
-	                		<>
-                				<Date date={this.state.day}/>
-	                			<IconButton onClick={this.leaveSingleDayMode.bind(this)}>
-		                            <ClearIcon />
-		                        </IconButton>
-	                        </>
-                		) : (                				
-                			<Select value={this.state.month} onChange={this.handleMonthSelect.bind(this)}>
-                				{this.state.months.map((month,i) => (
-                						<MenuItem key={i} value={i}>{
-                							["Januar","Februar","Maerz","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"][month.month - 1]
-                						}</MenuItem>
-                				))}
-                			</Select>
-                		)}
-                		{this.canGoRight() ? (
-                			<IconButton onClick={this.handleRight.bind(this)}>
+                            </IconButton>
+                    ) : null}
+                    {this.state.singleDayMode ? (
+                      <>
+                        <Date date={this.state.day}/>
+                        <IconButton onClick={this.leaveSingleDayMode.bind(this)}>
+                              <ClearIcon />
+                          </IconButton>
+                          </>
+                    ) : (
+                      <Select value={this.state.month} onChange={this.handleMonthSelect.bind(this)}>
+                        {this.state.months.map((month,i) => (
+                            <MenuItem key={i} value={i}>{
+                              ["Januar","Februar","Maerz","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"][month.month - 1]
+                            }</MenuItem>
+                        ))}
+                      </Select>
+                    )}
+                    {this.canGoRight() ? (
+                      <IconButton onClick={this.handleRight.bind(this)}>
                                 <NavigateNextIcon />
-                            </IconButton>	
-                		) : null}
-                		{this.state.needData ? this.state.needData.map((need, i) => (
-                            	<div style={{ borderBottom: (need.date.format('E') === '6' ? '3' : '1') + 'px solid #e0e0e0', marginTop:'10px' }} key={i}>
-	                                {!singleDayMode ? (
-	                                	<Typography variant="h5">
-	                                		<Date date={need.date}/>
-	                                		<WithPermission permission="ROLE_RIGHT_NEEDS_APPROVE">
-		                                		<IconButton onClick={() => this.enterSingleDayMode(need.date)}>
-	                                				<GroupAddIcon />
-	                                			</IconButton>
-	                                		</WithPermission>
-	                                	</Typography>
-	                                ) : null}
-	                                <Need sessionState={sessionState} need={need.CONSTRUCTION_WORKER} singleDayMode={this.state.singleDayMode} label="Bauhelfer" />
-	                                <Need sessionState={sessionState} need={need.STORE_KEEPER} singleDayMode={this.state.singleDayMode} label="Magaziner" />
-	                                <Need sessionState={sessionState} need={need.KITCHEN_HELPER} singleDayMode={this.state.singleDayMode} label="Küche" />
-	                                <Need sessionState={sessionState} need={need.CLEANER} singleDayMode={this.state.singleDayMode} label="Putzen" />
-		                            </div>
-	                    )) : (
-	                    	<CircularProgress/>
-	                    )}
-	                            
-                	</>
+                            </IconButton>
+                    ) : null}
+                    {this.state.needData ? this.state.needData.map((need, i) => (
+                              <div style={{ borderBottom: (need.date.format('E') === '6' ? '3' : '1') + 'px solid #e0e0e0', marginTop:'10px' }} key={i}>
+                                  {!singleDayMode ? (
+                                    <Typography variant="h5">
+                                      <Date date={need.date}/>
+                                      <WithPermission permission="ROLE_RIGHT_NEEDS_APPROVE">
+                                      <IconButton onClick={() => this.enterSingleDayMode(need.date)}>
+                                          <GroupAddIcon />
+                                        </IconButton>
+                                      </WithPermission>
+                                    </Typography>
+                                  ) : null}
+                                  <Need sessionState={sessionState} need={need.CONSTRUCTION_WORKER} singleDayMode={this.state.singleDayMode} label="Bauhelfer" />
+                                  <Need sessionState={sessionState} need={need.STORE_KEEPER} singleDayMode={this.state.singleDayMode} label="Magaziner" />
+                                  <Need sessionState={sessionState} need={need.KITCHEN_HELPER} singleDayMode={this.state.singleDayMode} label="Küche" />
+                                  <Need sessionState={sessionState} need={need.CLEANER} singleDayMode={this.state.singleDayMode} label="Putzen" />
+                              </div>
+                      )) : (
+                        <CircularProgress/>
+                      )}
+
+                  </>
                 ):(
-                	<>
-                		<CircularProgress size={15}/>
-                	</>
+                  <>
+                    <CircularProgress size={15}/>
+                  </>
                 )}
              </>
         );
@@ -193,13 +193,12 @@ const NeedsComponent = props => (
     <>
         <Helmet titleTemplate="Helfer - %s" />
         <NeedsProvider>
-	        <SessionContext.Consumer>
-	        	{sessionState => (
-	        			<StatefulNeedsComponent sessionState={sessionState} {...props} />
+          <SessionContext.Consumer>
+            {sessionState => (
+                <StatefulNeedsComponent sessionState={sessionState} {...props} />
                 )}
             </SessionContext.Consumer>
         </NeedsProvider>
     </>
 );
 export default requiresLogin(NeedsComponent);
-
