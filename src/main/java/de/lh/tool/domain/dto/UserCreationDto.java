560481dcbf90d7df252dd012c38d214825e38072
Paul Hofmann <p.h.o@web.de>
2019-05-17 01:38:59 +0200

    Integration test fix (Redefined constructor)

--- src/main/java/de/lh/tool/domain/dto/UserCreationDto.java
+++ src/main/java/de/lh/tool/domain/dto/UserCreationDto.java
@@ -1,12 +1,14 @@
 package de.lh.tool.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserCreationDto {
 	private String firstName;
 	private String lastName;
