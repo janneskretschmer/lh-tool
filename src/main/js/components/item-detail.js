a7a2d37aadf1d6f6673a95a5ba636b52a8fb4c06
kretschmer <jannes.kretschmer@heroes.eu>
2019-07-12 13:49:59 +0000

    show dummy implemented

--- src/main/js/components/item-detail.js
+++ src/main/js/components/item-detail.js
@@ -3,10 +3,20 @@ import { withStyles } from '@material-ui/core/styles';
 import { SessionContext } from '../providers/session-provider';
 import { withContext } from '../util';
 import ItemDisplayComponent from './item-display';
-import ItemEditComponent from './item-item-edit';
+import ItemEditComponent from './item-edit';
 import Button from '@material-ui/core/Button';
 
-//@withStyles(styles)
+const styles = theme => ({
+    bold: {
+        fontWeight: 'bold',
+    },
+    button: {
+        marginRight: theme.spacing.unit,
+    },
+
+});
+
+@withStyles(styles)
 @withContext('sessionState', SessionContext)
 export default class ItemDetailComponent extends React.Component {
 
@@ -24,34 +34,34 @@ export default class ItemDetailComponent extends React.Component {
     }
 
     render () {
+        const {classes} = this.props
       return (
         <>
             [Detail-Ansicht für {this.props.match.params.id}]
             {this.state.edit ? (
                 <>
                     <ItemEditComponent></ItemEditComponent>
-                    <Button variant="contained" type="submit" onClick={() => this.changeEditState(false)}>
+                    <Button variant="contained" className={classes.button} type="submit" onClick={() => this.changeEditState(false)}>
                             Speichern
                     </Button>
                 </>
             ):(
                 <>
                     <ItemDisplayComponent></ItemDisplayComponent>
-                    <Button variant="contained" onClick={() => this.changeEditState(true)}>
+                    <Button variant="contained" className={classes.button} onClick={() => this.changeEditState(true)}>
                         Bearbeiten
+                    </Button>
+                    <Button variant="contained" className={classes.button} onClick={() => alert('TODO: implement "Verschieben"')}>
+                        Verschieben
+                    </Button>
+                    <Button variant="contained" className={classes.button} onClick={() => alert('TODO: implement "Kopieren"')}>
+                        Kopieren
+                    </Button>
+                    <Button variant="contained" className={classes.button} onClick={() => alert('TODO: implement "Zerstörung"')}>
+                        Defekt
                     </Button>
                 </>
             )}
-            <span>Hammer</span>
-            Eindeutiger Bezeichner
-            (Barcode)
-            Menge+Einheit
-            Maße
-            Verbrauchsgegenstand
-            Beschreibung
-            Tags
-            Gewerke
-            Historie
         </>
       )
     }
