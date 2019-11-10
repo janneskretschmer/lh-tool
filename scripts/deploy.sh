39f67bf7228951cb9a0e5c8b82362878125f519e
kretschmer <jannes.kretschmer@heroes.eu>
2019-11-10 17:31:44 +0100

    deployment test 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
-ls -l
-pwd
-cat src/main/resources/docker/run.sh
 if [ "${1,,}" = "stage" ]; then
     echo "Executing stage deployment..."
     env="stage"
     path="stage"
+    db_url=$CREDENTIALS_STAGE_DB_URL
+    jwt_secret=$CREDENTIALS_STAGE_JWT_SECRET
+    base=$CREDENTIALS_STAGE_BASE
 elif [ "${1,,}" = "prod" ]; then
     echo "Executing prod deployment..."
     env="prod"
     path=""
+    db_url=$CREDENTIALS_PROD_DB_URL
+    jwt_secret=$CREDENTIALS_PROD_JWT_SECRET
+    base=$CREDENTIALS_PROD_BASE
 else
     echo "Fail! Please pass stage or prod as argument"
+    exit 1
 fi
 
+echo "jdbc.url=$db_url" > src/main/resources/credentials.properties
+echo "jdbc.username=$CREDENTIALS_DB_USERNAME" >> src/main/resources/credentials.properties
+echo "jdbc.password=$CREDENTIALS_DB_PW" >> src/main/resources/credentials.properties
+
+echo "app.jwtSecret=$jwt_secret" >> src/main/resources/credentials.properties
+echo "app.jwtExpirationInMs=$CREDENTIALS_JWT_EXPIRATION" >> src/main/resources/credentials.properties
+echo "app.base=$base" >> src/main/resources/credentials.properties
+echo "app.environment=$env" >> src/main/resources/credentials.properties
+
+echo "mail.smtp.host=$CREDENTIALS_SMTP_HOST" >> src/main/resources/credentials.properties
+echo "mail.smtp.password=$CREDENTIALS_SMTP_PW" >> src/main/resources/credentials.properties
+echo "mail.smtp.username=$CREDENTIALS_SMTP_USER" >> src/main/resources/credentials.properties
+echo "mail.smtp.tlsEnabled=true" >> src/main/resources/credentials.properties
+
+mvn war:war
 war_file="$(ls -t target/*.war | head -1)"
 
 ssh_key="$(mktemp)"
