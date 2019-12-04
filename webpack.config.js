3e4e09ae319f15dc6ab23d57ec0898a00437f30d
kretschmer <jannes.kretschmer@heroes.eu>
2019-12-04 20:47:37 +0100

    fixed codacy issues

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var PROD_BUILD = true;
 
 module.exports = {
-	entry : './src/main/js/browser.js',
-	devtool : PROD_BUILD ? 'cheap-module-source-map' : 'sourcemaps',
-	cache : true,
-	mode : PROD_BUILD ? 'production' : 'development',
-	output : {
-		path : __dirname,
-		filename : './src/main/resources/static/built/bundle.js'
+	entry: './src/main/js/browser.js',
+	devtool: PROD_BUILD ? 'cheap-module-source-map' : 'sourcemaps',
+	cache: true,
+	mode: PROD_BUILD ? 'production' : 'development',
+	output: {
+		path: __dirname,
+		filename: './src/main/resources/static/built/bundle.js'
 	},
 	plugins: PROD_BUILD ? [
 		new webpack.DefinePlugin({
@@ -20,7 +20,7 @@ module.exports = {
 				'NODE_ENV': JSON.stringify('production')
 			}
 		})
-	] : [ ],
+	] : [],
 	optimization: PROD_BUILD ? {
 		minimizer: [
 			new UglifyJsPlugin({
@@ -34,24 +34,24 @@ module.exports = {
 				sourceMap: false
 			})
 		]
-	} : { },
-	module : {
-		rules : [ {
-			test : path.join(__dirname, '.'),
+	} : {},
+	module: {
+		rules: [{
+			test: path.join(__dirname, '.'),
 			include: [
-			    path.resolve(__dirname, "src"),
-			    path.resolve(__dirname, "node_modules/superagent")
-			  ],
-			use : [ {
-				loader : 'babel-loader',
-				options : {
-					presets : [ '@babel/preset-env', '@babel/preset-react' ],
-					plugins : [
+				path.resolve(__dirname, 'src'),
+				path.resolve(__dirname, 'node_modules/superagent')
+			],
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react'],
+					plugins: [
 						['@babel/plugin-proposal-decorators', { legacy: true }],
 						['@babel/plugin-proposal-class-properties', { loose: true }],
 					],
 				}
-			} ]
-		} ]
+			}]
+		}]
 	}
 };
