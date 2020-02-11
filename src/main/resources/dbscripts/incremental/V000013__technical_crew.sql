d61d4f19255cfeb00877aec55dafffe315098841
kretschmer <jannes.kretschmer@heroes.eu>
2020-02-11 16:09:56 +0100

    hotfix for old mysql versions

--- src/main/resources/dbscripts/incremental/V000013__technical_crew.sql
+++ src/main/resources/dbscripts/incremental/V000013__technical_crew.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `technical_crew` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE(name)) ENGINE = InnoDB;
+CREATE TABLE `technical_crew` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`), UNIQUE(name)) ENGINE = InnoDB;
 ALTER TABLE `item` ADD `technical_crew_id` INT NOT NULL AFTER `picture_url`;
 ALTER TABLE `item` ADD FOREIGN KEY (`technical_crew_id`) REFERENCES `technical_crew`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
