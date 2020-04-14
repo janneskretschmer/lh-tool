59efe2e61c06c49af66a65e901a93155980c9547
Jannes Kretschmer <jannes.kretschmer@gmx.de>
2020-04-14 08:52:57 +0200

    fixed unit tests

--- src/test/java/de/lh/tool/config/security/JwtTokenProviderTest.java
+++ src/test/java/de/lh/tool/config/security/JwtTokenProviderTest.java
@@ -4,23 +4,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Base64;
 
+import org.apache.commons.logging.Log;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import de.lh.tool.domain.model.User;
 import de.lh.tool.domain.model.UserRole;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 
 public class JwtTokenProviderTest {
 	private JwtTokenProvider provider;
 
+	private Log log;
+
 	@BeforeEach
-	public void before() {
+	public void before() throws NoSuchFieldException, SecurityException, Exception {
 		provider = new JwtTokenProvider();
 		provider.setJwtSecret("secret");
 		provider.setJwtExpirationInMs(1000000);
+
+		log = Mockito.mock(Log.class);
+		setFinalStatic(provider.getClass().getDeclaredField("log"), log);
 	}
 
 	@Test
@@ -44,23 +55,34 @@ public class JwtTokenProviderTest {
 	@Test
 	public void testValidateTokenEmpty() {
 		assertFalse(provider.validateToken(null));
+		Mockito.verify(log, Mockito.times(1)).error(Mockito.any(), Mockito.any(IllegalArgumentException.class));
 	}
 
 	@Test
 	public void testValidateTokenExpired() {
 		provider.setJwtExpirationInMs(0);
 		assertFalse(provider.validateToken(getTestToken()));
+		Mockito.verify(log, Mockito.times(1)).error(Mockito.any(), Mockito.any(ExpiredJwtException.class));
 	}
 
 	@Test
 	public void testValidateTokenMalformed() {
 		assertFalse(provider.validateToken(getTestToken().substring(10)));
+		Mockito.verify(log, Mockito.times(1)).error(Mockito.any(), Mockito.any(MalformedJwtException.class));
 	}
 
 	public String getTestToken() {
 		return provider.generateToken(User.builder().id(123l)
 				.roles(Arrays.asList(new UserRole(null, null, "ROLE_TEST1"), new UserRole(null, null, "ROLE_TEST2")))
 				.build());
+	}
+
+	static void setFinalStatic(Field field, Object newValue) throws Exception {
+		field.setAccessible(true);
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+		field.set(null, newValue);
 	}
 
 }
