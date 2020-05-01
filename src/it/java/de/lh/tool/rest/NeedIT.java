54f8c3ac3782ba38a95eda0f4b34363841babe88
janneskretschmer <jannes.kretschmer@gmx.de>
2020-05-01 15:14:16 +0200

    fixed bug in need put endpoint (#95)

--- src/it/java/de/lh/tool/rest/NeedIT.java
+++ src/it/java/de/lh/tool/rest/NeedIT.java
@@ -144,6 +144,31 @@ public class NeedIT extends BasicRestIntegrationTest {
 	}
 
 	@Test
+	public void testNeedQuantityModificationOwn() throws Exception {
+		assertTrue(testEndpoint(EndpointTest.builder()//
+				.initializationQueries(List.of("INSERT INTO `helper_type` (`id`, `name`) VALUES (1, 'Test1')",
+						"INSERT INTO `project` (`id`, `name`, `start_date`, `end_date`) VALUES (1, 'Test1', '2020-04-09', '2020-04-24')",
+						"INSERT INTO project_user(project_id, user_id) SELECT 1,id FROM user",
+						"INSERT INTO project_helper_type (id, project_id, helper_type_id, weekday, start_time, end_time) VALUES (1, 1, 1, 1, '07:00:00', '17:00:00')",
+						"INSERT INTO need (id, project_helper_type_id, quantity, date) VALUES (1, 1, 42, '2020-04-23')"))
+				.url(REST_URL + "/needs/1").method(Method.PUT)
+				.body(NeedDto.builder().id(1l).projectHelperTypeId(1l).date(Date.valueOf("2020-04-23")).quantity(123)
+						.build())
+				.userTests(List.of(UserTest.builder()
+						.emails(List.of(ADMIN_EMAIL, CONSTRUCTION_SERVANT_EMAIL, LOCAL_COORDINATOR_EMAIL))
+						.expectedHttpCode(HttpStatus.OK)
+						.expectedResponse(
+								"{\"id\":1,\"projectHelperTypeId\":1,\"date\":1587600000000,\"quantity\":123,\"links\":[{\"rel\":\"self\",\"href\":\"http://localhost:8080/lh-tool/rest/needs/1\",\"hreflang\":null,\"media\":null,\"title\":null,\"type\":null,\"deprecation\":null}]}")
+						.validationQueries(List.of(
+								"SELECT * FROM need WHERE project_helper_type_id=1 AND quantity=123 AND date='2020-04-23'"))
+						.build()))
+				.httpCodeForOthers(HttpStatus.FORBIDDEN)
+				.validationQueriesForOthers(List
+						.of("SELECT * FROM need WHERE project_helper_type_id=1 AND quantity=42 AND date='2020-04-23'"))
+				.build()));
+	}
+
+	@Test
 	public void testNeedModificationDuplicate() throws Exception {
 		assertTrue(testEndpoint(EndpointTest.builder()//
 				.initializationQueries(List.of("INSERT INTO `helper_type` (`id`, `name`) VALUES (1, 'Test1')",
