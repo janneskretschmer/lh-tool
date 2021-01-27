8c0b9b76522a71f6bc396061c4e00b2c997784be
Jannes Kretschmer <jannes.kretschmer@gmx.de>
2021-01-27 10:17:46 +0100

    upgraded npm versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-var path = require('path');
-var webpack = require('webpack');
-var TerserPlugin = require('terser-webpack-plugin');
-var settings = require('./src/main/js/settings');
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
 
-var PROD_BUILD = settings.prodBuild;
+const settings = require('./src/main/js/settings');
+const PROD_BUILD = settings.prodBuild;
+const ENVIRONMENT = PROD_BUILD ? 'production' : 'development';
 
 module.exports = {
 	entry: './src/main/js/browser.js',
-	devtool: PROD_BUILD ? 'cheap-module-source-map' : 'sourcemaps',
+	devtool: PROD_BUILD ? false : 'inline-source-map',
 	cache: true,
-	mode: PROD_BUILD ? 'production' : 'development',
+	mode: ENVIRONMENT,
 	output: {
 		path: __dirname,
 		filename: './src/main/resources/static/built/bundle.js'
 	},
-	plugins: PROD_BUILD ? [
+	plugins: [
 		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		})
-	] : [],
+			'process.env.NODE_ENV': JSON.stringify(ENVIRONMENT),
+		}),
+		new webpack.ProvidePlugin({
+			process: 'process/browser',
+		}),
+	],
 	optimization: PROD_BUILD ? {
 		minimizer: [
 			new TerserPlugin({
-				cache: true,
 				parallel: true,
 				terserOptions: {
 					compress: true,
 					ecma: 6,
 					mangle: true
 				},
-				sourceMap: false
 			})
 		]
 	} : {},
@@ -53,5 +53,13 @@ module.exports = {
 				}
 			}]
 		}]
+	},
+	resolve: {
+		fallback: {
+			buffer: require.resolve("buffer/"),
+			crypto: require.resolve("crypto-browserify"),
+			util: require.resolve("util/"),
+			stream: require.resolve("stream-browserify"),
+		}
 	}
 };
